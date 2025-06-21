9fd6ec4bea993a7fa32aa41c7afeaa2ef6d4db23
whutao <whutao.dev@gmail.com>
2025-06-21 10:54:17 +0300

    Remove inefficient Sequence extenions.

--- Sources/SwiftExtras/Extensions/Sequence+KeyPaths.swift
+++ Sources/SwiftExtras/Extensions/Sequence+KeyPaths.swift
@@ -49,18 +49,6 @@ extension Sequence {
         }
     }
     
-    /// Returns the elements of the sequence, sorted in the increasing order using the given keypath.
-    @inlinable
-    public func sorted<Value: Comparable>(
-        by keyPath1: KeyPath<Element, Value>,
-        and keyPath2: KeyPath<Element, Value>,
-        ascending: Bool = true
-    ) -> [Element] {
-        return sorted(by: keyPath1, and: keyPath2) { lhs, rhs in
-            return ascending ? lhs < rhs : lhs > rhs
-        }
-    }
-    
     /// Returns the elements of the sequence, sorted using the given predicate as the comparison between elements and the keypath.
     @inlinable
     public func sorted<Value>(
@@ -69,22 +57,6 @@ extension Sequence {
     ) rethrows -> [Element] {
         return try sorted {
             try valuesAreInIncreasingOrder($0[keyPath: keyPath], $1[keyPath: keyPath])
-        }
-    }
-    
-    /// Returns the elements of the sequence, sorted using the given predicate as the comparison between elements, the keypath and the suplementary keypath.
-    @inlinable
-    public func sorted<Value: Comparable>(
-        by keyPath1: KeyPath<Element, Value>,
-        and keyPath2: KeyPath<Element, Value>,
-        using valuesAreInIncreasingOrder: (Value, Value) throws -> Bool
-    ) rethrows -> [Element] {
-        return try sorted {
-            if $0[keyPath: keyPath1] == $1[keyPath: keyPath1] {
-                return try valuesAreInIncreasingOrder($0[keyPath: keyPath2], $1[keyPath: keyPath2])
-            } else {
-                return try valuesAreInIncreasingOrder($0[keyPath: keyPath1], $1[keyPath: keyPath1])
-            }
         }
     }
 }
