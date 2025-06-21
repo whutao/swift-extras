00773cf576061babc9fb4aaaa4e261fd9a260cf9
whutao <whutao.dev@gmail.com>
2025-06-21 10:54:35 +0300

    Add fold for noncopyable Result type.

--- Sources/SwiftExtras/Extensions/Result+fold.swift
+++ Sources/SwiftExtras/Extensions/Result+fold.swift
@@ -16,3 +16,22 @@ extension Result {
         }
     }
 }
+
+extension Result where Success: ~Copyable {
+    
+    /// Transforms this result by applying one of two closures depending on whether it is a success or failure.
+    ///
+    /// - Parameters:
+    ///   - success: A closure to invoke if this `Result` is `.success`, receiving the wrapped success value.
+    ///   - failure: A closure to invoke if this `Result` is `.failure`, receiving the wrapped error value.
+    /// - Returns: The value returned by the invoked closure.
+    @inlinable
+    public consuming func fold<T>(success: (borrowing Success) -> T, failure: (Failure) -> T) -> T {
+        switch consume self {
+        case let .success(value):
+            return success(value)
+        case let .failure(value):
+            return failure(value)
+        }
+    }
+}
